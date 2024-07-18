79e6a88f0a5fc61d9f263779bd1a2baa586e92c5
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-18 18:44:31 +0300

    added signal handler

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,8 +4,6 @@ import sys
 import re
 import signal
 
-signal.signal(signal.SIGPIPE, signal.SIG_DFL)
-
 file_dict = {}
 file_size = 0
 line_size = 0
@@ -13,7 +11,29 @@ status_code = 0
 count = 0
 
 valid_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-pattern = r'(?P<ip>\d{1,3}(?:\.\d{1,3}){3}) - \[(?P<date>[^\]]+)\] "GET /projects/260 HTTP/1\.1" (?P<status>\d{3}) (?P<size>\d+)'
+pattern = (
+    r'(?P<ip>\d{1,3}(?:\.\d{1,3}){3}) - '
+    r'\[(?P<date>[^\]]+)\] '
+    r'"GET /projects/260 HTTP/1\.1" '
+    r'(?P<status>\d{3}) '
+    r'(?P<size>\d+)'
+)
+
+
+def print_stats():
+    """Print statistics"""
+    print("File size: {}".format(file_size))
+    for k, v in sorted(file_dict.items()):
+        print("{}: {}".format(k, v))
+
+
+def signal_handler(sig, frame):
+    """Handle keyboard interruption (CTRL + C)"""
+    print_stats()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
 
 
 for line in sys.stdin:
@@ -36,6 +56,6 @@ for line in sys.stdin:
 
         count += 1
         if count % 10 == 0:
-            print("File size: {}".format(file_size))
-            for k, v in sorted(file_dict.items()):
-                print("{}: {}".format(k, v))
+            print_stats()
+
+print_stats()
