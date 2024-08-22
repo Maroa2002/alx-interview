e3d8cf2351f44ef671c628906cdf3bfbcd17d76a
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-22 17:06:22 +0300

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module 0-making_change"""
+
+def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet a given total
+    """
+    if total <= 0:
+        return 0
+    
+    # Initialize dp array
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    
+    # Compute the fewest number of coins for each amount up to total
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+    
+    # If dp[total] is still inf, total can't be formed with coins available
+    return dp[total] if dp[total] != float('inf') else -1
